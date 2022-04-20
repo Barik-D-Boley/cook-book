36be9a4016c2003fd5fb71e811ef437e2ded5dfc
Barik-Boley <71157911+xBarikadex@users.noreply.github.com>
2022-04-20 15:23:13 -0700

    Added some details

--- src/components/RecipeCards.jsx
+++ src/components/RecipeCards.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import RecipeDNE from './RecipeDNE';
 // import { BiLinkExternal } from "react-icons/bi";
 
-const formatter = new Intl.ListFormat('en', { style: 'long', type: 'conjunction' });
+// const formatter = new Intl.ListFormat('en', { style: 'long', type: 'conjunction' });
+const pluralRegex = /(es|s)$/mi;
 
 function RecipeCards(recipes) {
     if (recipes.recipes.data.to === 0) return <RecipeDNE />
@@ -12,30 +13,33 @@ function RecipeCards(recipes) {
 
     let stuff = [];
     for (let i = 0; i < recipes.length; i++) {
-        let { image, label, calories, yield: servings, ingredientLines, ingredients,/*  totalTime, cautions, url  */} = recipes[i].recipe;
+        let { image, label, calories, yield: servings, ingredientLines, ingredients, cuisineType, mealType, dishType, totalTime, cautions, url } = recipes[i].recipe;
+
         let ingredientList = [];
         calories = Math.round(calories / 10) * 10;
+        cuisineType = cuisineType[0].charAt(0).toUpperCase() + cuisineType[0].slice(1);
+        if (dishType[0] === 'condiments and sauces') {
+            dishType = 'condiment or sauce';
+        } else if (dishType[0].match(pluralRegex)) {
+            dishType[0] = dishType[0].replace(pluralRegex, '');
+        }
         if (ingredientLines.length > 0) ingredientList = ingredientLines;
         else {
             ingredients.forEach(ing => {
                 ingredientList.push(ing.text);
             });
         }
+
         stuff.push(
             <div className='menuItem' key={i}>
                 <div className='img-container'><img className='image' src={image} alt={label} /></div>
                 <div className='textContainer'>
                     <div className='inlineTitle'>
-                        <h3 className='title'>{label}</h3>
-                        {/* <h3 className='title price'>{calories} Calories</h3>
-                        <h3 className='title price'>{servings} Serving{servings === 1 ? "" : "s"}</h3> */}
-                        <h3 className='title price'>{Math.round(calories/servings)} Calories per Serving</h3>
+                        <h3 className='title short-text'>{label}</h3>
+                        <h3 className='title price short-text'>{calories} Calories</h3>
                     </div>
                     <hr className='itemUnderline' />
-                    {/* <p className='desc'>{formatter.format(ingredientList)}</p> */}
-                    <p className='desc'>{calories} Calories</p>
-                    <p className='desc'>{servings} Serving{servings === 1 ? "" : "s"}</p>
-
+                    <p className="desc short-text">{label} is a{cuisineType.charAt(0).match(/[aeiou]/mi) ? "n" : ""} {cuisineType} {dishType} served during {mealType}. This recipe makes {servings} serving{servings === 1 ? "" : "s"} at {Math.round(calories/servings)} calories per serving. This dish takes {totalTime} minutes to cook, and has {ingredientList.length} {ingredientList.length === 1 ? "ingredient" : "different ingredients"}.</p>
                     <a href={`/individualRecipe`}>More Info</a>
                 </div>
             </div>
